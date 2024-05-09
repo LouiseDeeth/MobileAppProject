d00b51dd51d3836d1d4232ff0231447bba02d50c
LouiseDeeth <127743702+LouiseDeeth@users.noreply.github.com>
2024-05-09 14:56:50 +0100

    formatting and tidy up

--- src/app/weather/weather.page.ts
+++ src/app/weather/weather.page.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButton, IonItem, IonLabel, IonList, IonRadio, IonButtons, IonRadioGroup } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButton, IonItem, IonLabel, IonList, IonRadio, IonButtons, IonRadioGroup, IonTabs, IonIcon, IonTabBar, IonImg  } from '@ionic/angular/standalone';
 import { Storage } from '@ionic/storage-angular';
 import { Router, RouterModule } from '@angular/router';
 import { Geolocation } from '@capacitor/geolocation';
 import { DataService } from '../Services/data.service';
+import { addIcons } from 'ionicons';
+import { homeOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.page.html',
   styleUrls: ['./weather.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonBackButton, IonButton, IonItem, IonLabel, IonList, IonRadio, IonButtons, IonRadioGroup, RouterModule ]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonBackButton, IonButton, IonItem, IonLabel, IonList, IonRadio, IonButtons, IonRadioGroup, RouterModule, IonIcon, IonTabs, IonTabBar, IonImg ]
 })
 export class WeatherPage implements OnInit {
   myWeather: string = "";
@@ -20,10 +22,14 @@ export class WeatherPage implements OnInit {
   lat: string = ""; 
   long: String = "";
 
-constructor(private storage: Storage, private router: Router, private dataService: DataService) { }
-ngOnInit() {
+constructor(private storage: Storage, private router: Router, private dataService: DataService) {
+  addIcons({ 'home-outline': homeOutline });
 }
 
+ngOnInit(): void {
+}
+
+//store the weather - Galway is default if no stored weather
 async ionViewWillEnter() {
   await this.storage.create();
   const storedWeather = await this.storage.get('weather');
@@ -34,12 +40,14 @@ async ionViewWillEnter() {
   }
 }
 
+//get current location using geolocation
 async getCurrentLocation() {
   const coordinates = await Geolocation.getCurrentPosition();
   console.log('Current position:', coordinates.coords.latitude, coordinates.coords.longitude);
   return coordinates.coords;
 }
 
+//show the weather by location
 async showWeather() {
   try {
     const coords = await this.getCurrentLocation();
